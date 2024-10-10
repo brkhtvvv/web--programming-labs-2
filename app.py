cfb13b3e27ecc476d177be754c8fe04f785e5fed
Ольга Бархатова <barkhatova.olya@mail.ru>
2024-10-11 01:43:49 +0700

    Цикл в шаблоне

--- app.py
+++ app.py
@@ -176,11 +176,19 @@ def add_flower(name):
 </html>
 '''
 
-
 @app.route('/lab2/example')
 def example ():
     name = 'Бархатова Ольга'
     numberLab = '2'
     groupStudent = 'ФБИ-24'
     numberCourse = '3 курс'
-    return render_template('example.html')
+    fruits = [
+        {'name': 'яблоки', 'price': 100},
+        {'name': 'груши', 'price': 120},
+        {'name': 'апельсины', 'price': 80},
+        {'name': 'мандарины', 'price': 95},
+        {'name': 'манго', 'price': 321}
+    ]
+    return render_template('example.html', 
+                           name=name, numberLab=numberLab, groupStudent=groupStudent, 
+                           numberCourse=numberCourse, fruits=fruits)
