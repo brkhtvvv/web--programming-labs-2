ba19e4087996eaea7d1a36cd0b80e084024e7ee7
Ольга Бархатова <barkhatova.olya@mail.ru>
2024-10-25 20:29:08 +0700

    Покупка билетов на игры КХЛ

--- lab3.py
+++ lab3.py
@@ -122,3 +122,45 @@ def clear_cookie():
             response.set_cookie(cookie, '', expires=0)
     
     return response
+
+
+khl_teams = [
+    {'name': 'СКА Санкт-Петербург', 'city': 'Санкт-Петербург', 'ticket_price': 2500, 'founded': 1946},
+    {'name': 'ЦСКА Москва', 'city': 'Москва', 'ticket_price': 2400, 'founded': 1946},
+    {'name': 'Ак Барс', 'city': 'Казань', 'ticket_price': 2200, 'founded': 1956},
+    {'name': 'Металлург Магнитогорск', 'city': 'Магнитогорск', 'ticket_price': 2000, 'founded': 1955},
+    {'name': 'Авангард Омск', 'city': 'Омск', 'ticket_price': 2300, 'founded': 1950},
+    {'name': 'Динамо Москва', 'city': 'Москва', 'ticket_price': 2100, 'founded': 1946},
+    {'name': 'Локомотив Ярославль', 'city': 'Ярославль', 'ticket_price': 1900, 'founded': 1959},
+    {'name': 'Салават Юлаев', 'city': 'Уфа', 'ticket_price': 1800, 'founded': 1961},
+    {'name': 'Трактор Челябинск', 'city': 'Челябинск', 'ticket_price': 1700, 'founded': 1947},
+    {'name': 'Спартак Москва', 'city': 'Москва', 'ticket_price': 2000, 'founded': 1946},
+    {'name': 'Амур', 'city': 'Хабаровск', 'ticket_price': 1500, 'founded': 1966},
+    {'name': 'Барыс', 'city': 'Астана', 'ticket_price': 1600, 'founded': 1999},
+    {'name': 'Северсталь', 'city': 'Череповец', 'ticket_price': 1400, 'founded': 1956},
+    {'name': 'Нефтехимик', 'city': 'Нижнекамск', 'ticket_price': 1300, 'founded': 1968},
+    {'name': 'Адмирал', 'city': 'Владивосток', 'ticket_price': 1700, 'founded': 2013},
+    {'name': 'Динамо Минск', 'city': 'Минск', 'ticket_price': 1600, 'founded': 1976},
+    {'name': 'Куньлунь Ред Стар', 'city': 'Пекин', 'ticket_price': 1800, 'founded': 2016},
+    {'name': 'Сибирь Новосибирск', 'city': 'Новосибирск', 'ticket_price': 1500, 'founded': 1962},
+    {'name': 'Витязь Подольск', 'city': 'Подольск', 'ticket_price': 1400, 'founded': 1996},
+    {'name': 'Торпедо Нижний Новгород', 'city': 'Нижний Новгород', 'ticket_price': 1600, 'founded': 1947}
+]
+
+
+@lab3.route('/lab3/search')
+def search():
+    min_price = request.args.get('min_price')
+    max_price = request.args.get('max_price')
+    if min_price is None and max_price is None:
+        return render_template('/lab3/search.html')
+    min_price = float(min_price) if min_price else 0
+    max_price = float(max_price) if max_price else float('inf')
+    # Фильтруем автомобили по диапазону цен
+    filtered_khl_teams = [khl for khl in khl_teams if min_price <= khl['ticket_price'] <= max_price]
+    # Отображаем результат поиска
+    return render_template('/lab3/results.html', khl=filtered_khl_teams, min_price=min_price, max_price=max_price)
+# Страница результатов
+@lab3.route('/lab3/results')
+def results():
+    return render_template('lab3/results.html')
