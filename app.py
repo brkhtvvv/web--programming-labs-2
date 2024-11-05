aa6d99fd48f258c815309b738e8fb66ef56e6e9a
Ольга Бархатова <barkhatova.olya@mail.ru>
2024-11-05 22:34:51 +0700

    Заготовка к 4-ой лабораторной

--- app.py
+++ app.py
@@ -2,11 +2,13 @@ from flask import Flask, redirect, url_for, render_template
 from lab1 import lab1
 from lab2 import lab2
 from lab3 import lab3
+from lab4 import lab4
 
 app = Flask (__name__)
 app.register_blueprint(lab1)
 app.register_blueprint(lab2)
 app.register_blueprint(lab3)
+app.register_blueprint(lab4)
 
 @app.route("/")
 @app.route("/index")
@@ -30,6 +32,7 @@ def menu():
     <li><a href="http://127.0.0.1:5000/lab1">Первая лабораторная</a></li>
     <li><a href="http://127.0.0.1:5000/lab2">Вторая лабораторная</a></li>
     <li><a href="http://127.0.0.1:5000/lab3">Третья лабораторная</a></li>
+    <li><a href="http://127.0.0.1:5000/lab4">Четвертая лабораторная</a></li>
     <footer>
         &copy: Бархатова Ольга, ФБИ-24, 3 курс, 2024
     </footer>
