52c92becd9baf7650139fd028c379ceea265ddc7
Ольга Бархатова <barkhatova.olya@mail.ru>
2024-11-06 11:58:01 +0700

    Счетчик деревьев

--- lab4.py
+++ lab4.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, url_for
+from flask import Blueprint, render_template, request, url_for, redirect
 lab4 = Blueprint('lab4', __name__)
  
 @lab4.route('/lab4/')
@@ -84,3 +84,18 @@ def stepen():
     
     result = x1 ** x2
     return render_template('lab4/stepen.html', x1=x1, x2=x2, result=result, back_url=url_for('lab4.stepen'))
+
+tree_count = 0
+@lab4.route('/lab4/tree', methods = ['GET', 'POST'])
+def tree():
+    global tree_count
+    if request.method == 'GET':
+        return render_template('lab4/tree.html', tree_count=tree_count)
+    
+    operation = request.form.get('operation')
+    if operation == 'cut':
+        tree_count -= 1
+    elif operation == 'plant':
+        tree_count +=1
+    
+    return redirect ('/lab4/tree')
