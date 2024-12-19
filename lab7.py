28c061b3936eb3dfc268ab51390bf876adab9129
Ольга Бархатова <barkhatova.olya@mail.ru>
2024-12-19 16:47:41 +0700

    Дополнительное задание

--- lab7.py
+++ lab7.py
@@ -1,97 +1,125 @@
-from flask import Blueprint, render_template, request, abort, jsonify
+from flask import Blueprint, render_template, request, abort, jsonify, abort, current_app
+import psycopg2
+from psycopg2.extras import RealDictCursor
 
 lab7 = Blueprint('lab7', __name__)
+
+def db_connect():
+        conn = psycopg2.connect (
+            host = '127.0.0.1',
+            database = 'olya_barkhatova_knowledge_base',
+            user = 'olya_barkhatova_knowledge_base',
+            password = '123'
+    )
+        cur = conn.cursor(cursor_factory=RealDictCursor)
+        return conn, cur
+
+def db_close(conn, cur):
+    conn.commit()
+    cur.close()
+    conn.close()
 
 @lab7.route('/lab7/')
 def main ():
     return render_template('lab7/lab7.html')
 
-films = [
-    {
-        "title": "Intouchables",
-        "title_ru": "1+1",
-        "year": 2011,
-        "description": "Аристократ на коляске нанимает в сиделки бывшего заключенного. \
-        Искрометная французская комедия с Омаром Си"
-    },
-    {
-        "title": "Interstellar",
-        "title_ru": "Интерстеллар",
-        "year": 2014,
-        "description": "Когда засуха, пыльные бури и вымирание растений приводят человечество \
-        к продовольственному кризису, коллектив исследователей и учёных отправляется сквозь \
-        червоточину (которая предположительно соединяет области пространства-времени через большое расстояние) \
-        в путешествие, чтобы превзойти прежние ограничения для космических путешествий человека и найти планету с \
-        подходящими для человечества условиями."
-    },
-    {
-        "title": "The Gentlemen",
-        "title_ru": "Джентльмены",
-        "year": 2019,
-        "description": "Один ушлый американец ещё со студенческих лет приторговывал наркотиками, а теперь придумал схему \
-        нелегального обогащения с использованием поместий обедневшей английской аристократии и очень неплохо на этом разбогател. \
-        Другой пронырливый журналист приходит к Рэю, правой руке американца, и предлагает тому купить киносценарий, в котором подробно \
-        описаны преступления его босса при участии других представителей лондонского криминального мира — партнёра-еврея, китайской диаспоры, \
-        чернокожих спортсменов и даже русского олигарха."
-    },
-    {
-        "title": "Inception",
-        "title_ru": "Начало",
-        "year": 2010,
-        "description": "Кобб – талантливый вор, лучший из лучших в опасном искусстве извлечения: он крадет ценные секреты из глубин подсознания во \
-        время сна, когда человеческий разум наиболее уязвим. Редкие способности Кобба сделали его ценным игроком в привычном к предательству мире промышленного \
-        шпионажа, но они же превратили его в извечного беглеца и лишили всего, что он когда-либо любил."
-    },
-    {
-        "title": "The Wolf of Wall Street",
-        "title_ru": "Волк с Уолл-стрит",
-        "year": 2013,
-        "description": "1987 год. Джордан Белфорт становится брокером в успешном инвестиционном банке. Вскоре банк закрывается после внезапного обвала индекса Доу-Джонса.\
-        По совету жены Терезы Джордан устраивается в небольшое заведение, занимающееся мелкими акциями. Его настойчивый стиль общения с клиентами и врождённая харизма \
-        быстро даёт свои плоды. Он знакомится с соседом по дому Донни, торговцем, который сразу находит общий язык с Джорданом и решает открыть с ним собственную фирму. \
-        В качестве сотрудников они нанимают нескольких друзей Белфорта, его отца Макса и называют компанию «Стрэттон Оукмонт». В свободное от работы время Джордан прожигает \
-        жизнь: лавирует от одной вечеринки к другой, вступает в сексуальные отношения с проститутками, употребляет множество наркотических препаратов, в том числе кокаин и кваалюд. \
-        Однажды наступает момент, когда быстрым обогащением Белфорта начинает интересоваться агент ФБР..."
-    }
-]
 @lab7.route('/lab7/rest-api/films/', methods=['GET'])
 def get_films():
+    conn, cur = db_connect()
+    cur.execute("SELECT * FROM films")
+    films = cur.fetchall()
+    db_close(conn, cur)
     return jsonify(films)
 
 @lab7.route('/lab7/rest-api/films/<int:id>', methods=['GET'])
 def get_film(id):
-    if id < 0 or id >= len(films):
+    conn, cur = db_connect()
+    if current_app.config.get('DB_TYPE') == 'postgres':
+        cur.execute("SELECT * FROM films WHERE id = %s", (id,))
+    else:  
+        cur.execute("SELECT * FROM films WHERE id = ?", (id,))
+    film = cur.fetchone()
+    db_close(conn, cur)
+    
+    if not film:
         abort(404)
-    return jsonify(films[id])
+    return jsonify(film)
 
 @lab7.route('/lab7/rest-api/films/<int:id>', methods=['DELETE'])
 def del_film(id):
-    if id < 0 or id >= len(films):
+    conn, cur = db_connect()
+    cur.execute("DELETE FROM films WHERE id = %s RETURNING *", (id,))
+    deleted_film = cur.fetchone()
+    db_close(conn, cur)
+    
+    if not deleted_film:
         abort(404)
-    del films[id]
     return '', 204
 
 @lab7.route('/lab7/rest-api/films/<int:id>', methods=['PUT'])
 def put_film(id):
-    if id < 0 or id >= len(films):
-        abort(404)
     film = request.get_json()
-    if film ['description'] == '':
+    
+    if not film.get('description', ''):
         return jsonify({'description': 'Заполните описание'}), 400
-    if not film.get('title'):
-        film['title'] = film['title_ru']
-    films[id] = film
-    jsonify(films[id])
+    elif len(film['description']) > 2000:
+        return jsonify({'description': 'Описание не должно превышать 2000 символов'}), 400
+    if not film.get('title') and not film.get('title_ru'):
+        return jsonify({'title': 'Заполните поля с названиями'}), 400
+    if not film.get('title_ru'):
+        return jsonify({'title_ru': 'Заполните русское название'}), 400
+    
+    if not film.get('year'):
+        return jsonify({'year': 'Укажите год выпуска фильма'}), 400
+    elif not str(film['year']).isdigit() or int(film['year']) < 1800 or int(film['year']) > 2100:
+        return jsonify({'year': 'Введите корректный год (1800-2100)'}), 400
+
+    conn, cur = db_connect()
+  
+    if current_app.config.get('DB_TYPE') == 'postgres':
+        cur.execute("""
+            UPDATE films
+            SET title = %s, title_ru = %s, year = %s, description = %s
+            WHERE id = %s RETURNING *
+        """, (film['title'], film['title_ru'], film['year'], film['description'], id))
+    else:  
+        cur.execute("""
+            UPDATE films
+            SET title = ?, title_ru = ?, year = ?, description = ?
+            WHERE id = ? RETURNING *
+        """, (film['title'], film['title_ru'], film['year'], film['description'], id))
+ 
+    updated_film = cur.fetchone()
+    db_close(conn, cur)
+    
+    if not updated_film:
+        abort(404)
+    
+    return jsonify(updated_film)
 
 @lab7.route('/lab7/rest-api/films/', methods=['POST'])
 def add_film():
     film = request.get_json()
-    if not film:
-        abort(400)
-    if film.get('description', '') == '':
+    if not film.get('description', ''):
         return jsonify({'description': 'Заполните описание'}), 400
-    if not film.get('title'):
-        film['title'] = film['title_ru']
-    films.append(film)
-    return jsonify(film), 201
+    elif len(film['description']) > 2000:
+        return jsonify({'description': 'Описание не должно превышать 2000 символов'}), 400
+    if not film.get('title') and not film.get('title_ru'):
+        return jsonify({'title': 'Заполните поля с названиями'}), 400
+    if not film.get('title_ru'):
+        return jsonify({'title_ru': 'Заполните русское название'}), 400
+    
+    if not film.get('year'):
+        return jsonify({'year': 'Укажите год выпуска фильма'}), 400
+    elif not str(film['year']).isdigit() or int(film['year']) < 1800 or int(film['year']) > 2100:
+        return jsonify({'year': 'Введите корректный год (1800-2100)'}), 400
+    conn, cur = db_connect()
+    cur.execute("""
+        INSERT INTO films (title, title_ru, year, description)
+        VALUES (%s, %s, %s, %s) RETURNING *
+    """, (film['title'], film['title_ru'], film['year'], film['description']))
+    new_film = cur.fetchone()
+    db_close(conn, cur)
+    
+    return jsonify(new_film), 201
 
--- static/lab7/main.js
+++ static/lab7/main.js
@@ -21,13 +21,13 @@ function fillFilmList() {
             let editButton = document.createElement('button');
             editButton.innerText = 'Редактировать';
             editButton.onclick = function() {
-                editFilm(i);
+                editFilm(films[i].id);
             };
 
             let delButton = document.createElement('button');
             delButton.innerText = 'Удалить';
             delButton.onclick = function() {
-                deleteFilm(i, films[i].title_ru);
+                deleteFilm(films[i].id, films[i].title_ru);
             }
 
             tdActions.appendChild(editButton);
@@ -54,6 +54,9 @@ function deleteFilm(id, title) {
 function showModal() {
     document.querySelector('div.modal').style.display = 'block';
     document.getElementById('description-error').innerText = '';
+    document.getElementById('title-error').innerText = '';
+    document.getElementById('title_ru-error').innerText = '';
+    document.getElementById('year-error').innerText = '';
 }
 function hideModal() {
     document.querySelector('div.modal').style.display = 'none'
@@ -95,6 +98,14 @@ function sendFilm() {
     .then (function(errors) {
         if (errors.description)
             document.getElementById('description-error').innerText = errors.description;
+        if (errors.title)
+            document.getElementById('title-error').innerText = errors.title;
+        
+        if (errors.title_ru)
+            document.getElementById('title_ru-error').innerText = errors.title_ru;
+        
+        if (errors.year)
+            document.getElementById('year-error').innerText = errors.year;
     });
     }
 
@@ -105,10 +116,11 @@ function editFilm(id) {
     })
     .then (function (film){
         document.getElementById('id').value = id;
-        document.getElementById('title').value = film.title;
-        document.getElementById('title_ru').value = film.title_ru;
-        document.getElementById('year').value = film.year;
-        document.getElementById('description').value = film.description;
+        document.getElementById('title').value = film.title || '';
+        document.getElementById('title_ru').value = film.title_ru || '';
+        document.getElementById('year').value = film.year || '';
+        document.getElementById('description').value = film.description || '';
+
         showModal();
     })
 }
