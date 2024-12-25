d2ddb06bf646d71d3b5567272b48b02ce1b42198
Ольга Бархатова <barkhatova.olya@mail.ru>
2024-12-25 19:04:30 +0700

    модель базы данных

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 from flask import Flask, redirect, url_for, render_template
 import os
+from os import path
+from flask_sqlalchemy import SQLAlchemy
+from db import db
+from urllib.parse import quote
 from lab1 import lab1
 from lab2 import lab2
 from lab3 import lab3
@@ -13,6 +17,19 @@ app = Flask (__name__)
 
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'Секретно-секретный секрет')
 app.config['DB_TYPE'] = os.getenv('DB_TYPE', 'postgres')
+
+if app.config['DB_TYPE'] == 'postgres':
+    db_user = 'olya_barkhatova_own'
+    db_password = '123'
+    db_name = 'olya_barkhatova_own'
+    host_ip = '127.0.0.1'
+    host_port = 5432
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{db_user}:{db_password}@{host_ip}:{host_port}/{db_name}'
+else:
+    dir_path = path.dirname(path.realpath(__file__))
+    db_path = path.join(dir_path, "olya_barkhatova_own.db")
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///{db_path}'
+db.init_app(app)
 
 app.register_blueprint(lab1)
 app.register_blueprint(lab2)
